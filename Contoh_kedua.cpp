e886f301115b70f49f9edc66061944b143cefcc0
LENOVO <MeutyaAzzahrani>
2024-03-18 15:40:26 +0700

    Prosedur Fungsi dan Pengulangan

--- Contoh_kedua.cpp
+++ Contoh_kedua.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int main(){
-
-int bilangan;
+    int bilangan;
 
 srand(time(0));
 
 bilangan = rand() % 12;
 
-while(bilangan % 12){
+while(bilangan % 2){
     cout << bilangan << endl;
     cout << "Maka bilangannya ganjil" << endl;
 
